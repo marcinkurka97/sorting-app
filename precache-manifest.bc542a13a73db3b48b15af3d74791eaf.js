6e46d8492b884fd604f1667067490c1c4e8cd500
Marcin Kurka <marcinkurka97@gmail.com>
2019-12-27 16:12:35 +0100

    Updates

---
+++ precache-manifest.bc542a13a73db3b48b15af3d74791eaf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ba28a45815270c4f1f06c5af30d9c97",
+    "url": "/sorting-app/index.html"
+  },
+  {
+    "revision": "ae93a562f2db6499013d",
+    "url": "/sorting-app/static/css/main.0a82b5a6.chunk.css"
+  },
+  {
+    "revision": "089681eec66d26c5b746",
+    "url": "/sorting-app/static/js/2.bfb4b6de.chunk.js"
+  },
+  {
+    "revision": "ae93a562f2db6499013d",
+    "url": "/sorting-app/static/js/main.59202c1a.chunk.js"
+  },
+  {
+    "revision": "67eb806b21cbaff50a5d",
+    "url": "/sorting-app/static/js/runtime~main.e09a1a5f.js"
+  }
+]);
